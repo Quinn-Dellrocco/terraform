52ec55d3fe79cb26dc1c83907aa402b6905bba65
quinnslab <dsfjghlasdr@gmail.com>
2026-02-17 22:57:08 -0800

    3. updated cloud-init and qemu agent

---
+++ ubuntu/main.tf
@@ -0,0 +1,39 @@
+resource "proxmox_virtual_environment_vm" "test_vm" {
+  name      = "tf-test-04"
+  node_name = "pve1"
+
+  # Clone from your template VM
+  clone {
+    vm_id = 9000
+  }
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 10
+  }
+
+  network_device {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  initialization {
+    user_account {
+      username = "ubuntu"
+      keys     = [file("~/.ssh/id_ed25519.pub")]
+    }
+  }
+}
---
+++ ubuntu/outputs.tf
@@ -0,0 +1,4 @@
+output "vm_name" {
+  value = proxmox_virtual_environment_vm.test_vm.name
+}
+
